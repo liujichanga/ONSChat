44f8522d789b9bde464a7dc6afa17b87dabfb6de
wanglei <605455102@qq.com>
2016-12-03 17:10:45 +0800

    发布动态

--- ONSChat/Classes/Controllers/Me/MyDynamic/NewDynamicViewController.m
+++ ONSChat/Classes/Controllers/Me/MyDynamic/NewDynamicViewController.m
@@ -8,7 +8,7 @@
 
 #import "NewDynamicViewController.h"
 
-@interface NewDynamicViewController ()
+@interface NewDynamicViewController ()<UIActionSheetDelegate,UIImagePickerControllerDelegate,UINavigationControllerDelegate,UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *dyTextField;
 @property (weak, nonatomic) IBOutlet UIButton *dyAddImageBtn;
 
@@ -19,6 +19,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    UIBarButtonItem *rightItem=[[UIBarButtonItem alloc] initWithTitle:@"发布" style:UIBarButtonItemStylePlain target:self action:@selector(rightItemClick)];
+    self.navigationItem.rightBarButtonItem=rightItem;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -26,14 +28,66 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+//添加图片
+- (IBAction)dyAddImageBtnClick:(id)sender {
+    
+    [self.dyTextField resignFirstResponder];
+    UIActionSheet *sheet = [[UIActionSheet alloc] initWithTitle:nil
+                                                       delegate:self
+                                              cancelButtonTitle:@"取消"
+                                         destructiveButtonTitle:nil
+                                              otherButtonTitles:@"拍照", @"从相册选择", nil];
+    [sheet showInView:self.view];
+    
 }
-*/
+//发布动态
+-(void)rightItemClick{
+    
+    
+}
 
+#pragma mark - Delegate
+- (void)actionSheet:(UIActionSheet *)actionSheet didDismissWithButtonIndex:(NSInteger)buttonIndex{
+    
+    if (buttonIndex == 0) {
+        [self openImagePickerControllerWithScourceType:UIImagePickerControllerSourceTypeCamera];
+    } else if (buttonIndex == 1) {
+        [self openImagePickerControllerWithScourceType:UIImagePickerControllerSourceTypePhotoLibrary];
+    }
+}
+
+- (void)openImagePickerControllerWithScourceType:(UIImagePickerControllerSourceType)sourceType {
+    
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    [picker setValue:@(UIStatusBarStyleLightContent) forKey:@"_previousStatusBarStyle"];
+    picker.sourceType = sourceType;
+    picker.delegate = self;
+    picker.allowsEditing = YES;
+    [self presentViewController:picker animated:YES completion:nil];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+-(void)imagePickerController:(UIImagePickerController*)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    
+    // 关闭Picker
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    
+    // 原图
+    UIImage *image = [info objectForKey:UIImagePickerControllerEditedImage];
+    if (picker.sourceType == UIImagePickerControllerSourceTypeCamera)
+    {
+        UIImageWriteToSavedPhotosAlbum(image, self, nil, nil);
+    }
+    
+    [self.dyAddImageBtn setImage:image forState:UIControlStateNormal];
+}
+
+//当用户按下return键或者按回车键，keyboard消失
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
 @end
